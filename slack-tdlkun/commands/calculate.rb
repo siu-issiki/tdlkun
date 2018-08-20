2a587e0ae7ad04f1706f951d46212f1e6b827aca
Tomoki Tanezaki <siu.issiki@gmail.com>
2018-08-20 23:18:59 +0900

    add dentaku

--- slack-tdlkun/commands/calculate.rb
+++ slack-tdlkun/commands/calculate.rb
@@ -1,9 +1,20 @@
 module SlackTdlkunBot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
-      command 'calculate' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+      operator '='
+      command 'calculate'
+
+      def self.call(client, data, match)
+        result = Dentaku::Calculator.new.evaluate(match[:expression]) if match.names.include?('expression')
+        result = result.to_s if result
+        if result && result.length > 0
+          client.say(channel: data.channel, text: result)
+        else
+          client.say(channel: data.channel, text: 'Got nothing.', gif: 'nothing')
+        end
+      rescue StandardError => e
+        client.say(channel: data.channel, text: "Sorry, #{e.message}.", gif: 'idiot')
       end
     end
-  end
+end
 end
